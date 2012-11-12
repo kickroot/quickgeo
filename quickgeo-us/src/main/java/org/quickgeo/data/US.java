750321a7cc1a8e85225db35a8941d3aaea79bb49
Jason Nichols <jason@kickroot.com>
2012-11-12 10:54:07 -0500

    Initial commits

--- quickgeo-us/src/main/java/org/quickgeo/data/US.java
+++ quickgeo-us/src/main/java/org/quickgeo/data/US.java
@@ -1,3 +1,10 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+ */
+
+
 package org.quickgeo.data;
 
 import java.io.InputStream;
@@ -7,6 +14,7 @@ import org.quickgeo.PostalSource;
  *
  */
 public class US implements PostalSource {
+  
   ///////////////////////////// Class Attributes \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
   
   ////////////////////////////// Class Methods \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
